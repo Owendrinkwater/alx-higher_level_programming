75de3098aa7b4c90f8915c54ba47738d7a50b543
Owendrinkwater <owenowade@gmail.com>
2025-07-11 00:42:47 +0300

    List all shows and their genre ID (NULL if no genre)

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- 12. Genre ID for all shows
+-- List all shows and their genre ID (NULL if no genre)
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+        LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+    ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
