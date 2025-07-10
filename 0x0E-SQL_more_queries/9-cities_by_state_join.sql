9af4dc5c7bae9f4800481162f3c3e33716bd27e5
Owendrinkwater <owenowade@gmail.com>
2025-07-10 14:30:57 +0300

    List all cities with their corresponding state names

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- List all cities with their corresponding state names
+
+SELECT cities.id, cities.name, states.name
+    FROM cities
+        INNER JOIN states
+        ON cities.state_id = states.id
+ORDER BY cities.id ASC;
